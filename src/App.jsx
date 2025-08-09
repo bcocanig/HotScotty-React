c927c04e527f0a849aa6af64ccff0046de6fcf20
brandon <bcocanig@gmail.com>
2025-08-08 19:27:36 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import HotScottyWebsite from './HotScottyWebsite.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <HotScottyWebsite />
+    </>
+  );
+}
+
+export default App;
